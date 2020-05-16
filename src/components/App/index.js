a83929db4e31375f0fa6ee789e0b770aab0fa481
Igor Bykov <igor.bykov@protonmail.com>
2020-05-16 11:28:56 +0200

    Initial set-up

---
+++ src/components/App/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { connect } from 'react-redux'
+import overlay from './../../redux/slices/overlay';
+import Dashboard from './../Dashboard';
+import ErrorPage from './../ErrorPage';
+
+const App = ({ dispatch }) => {
+  return (
+    <Router>
+        <nav>
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="/about/">About</Link>
+            <br />
+            <button
+                onClick={() => {
+                    dispatch(overlay.actions.show())
+                    setTimeout(
+                        () => dispatch(overlay.actions.hide()), 
+                        3000
+                    )
+                }}
+            >
+                Show Overlay 3 seconds
+            </button>
+        </nav>
+        <Switch>
+            <Route exact path="/">
+                <Dashboard />
+            </Route>
+            <Route path="/about/">
+                <h1>About us!</h1>
+            </Route>
+            <Route path="*">
+                <ErrorPage
+                    title="404"
+                    message="Page not found"
+                />
+            </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+export default connect()(App);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import overlaySlice from './../slices/overlay';
+
+export default combineReducers({
+    overlay: overlaySlice.reducer
+});
